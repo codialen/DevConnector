d6e6f310fba94f6c1050d4858657fdbcaf80054a
Codi Alen <codi.alen@gmail.com>
2021-05-06 19:41:43 -0700

    prepare application for deployment to heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express'); // imports express & gets a very simple express server running
-const connectDB = require ('./config/db');
+const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express(); // initialize our app variable w/ express
 
@@ -9,7 +10,6 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
 // creates a single endpoint "/" to test if our API works
 // the user makes a get request to (go into) this page & we'll put in a callback function => res.send()
 // which sends data to the browser
@@ -19,6 +19,16 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 // this process.env.PORT will look for an environemtn variable called PORT to use & when we deploy to heroku
